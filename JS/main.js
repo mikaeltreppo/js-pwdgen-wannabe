1af43031e57cf0996936e196f0cb6aaf9bf70ced
mikaeltreppo <mikaeltreppo@outlook.com>
2023-02-21 15:58:12 +0100

    Fine

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+let myName= prompt('Inserisci il tuo nome');
+let mySurname=prompt('Inserisci il tuo cognome');
+let myColor= prompt('Inserisci il tuo colore prefdrito');
+
+document.getElementById('password').innerHTML= myName + mySurname + myColor + '23';
