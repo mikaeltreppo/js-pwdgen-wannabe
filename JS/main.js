3ee00a01115c178b6fcf5c9b42577a1b1f7cbab9
mikaeltreppo <mikaeltreppo@outlook.com>
2023-02-21 15:59:34 +0100

    Fine esercizio

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,5 @@
 let myName= prompt('Inserisci il tuo nome');
 let mySurname=prompt('Inserisci il tuo cognome');
-let myColor= prompt('Inserisci il tuo colore prefdrito');
+let myColor= prompt('Inserisci il tuo colore preferito');
 
 document.getElementById('password').innerHTML= myName + mySurname + myColor + '23';
